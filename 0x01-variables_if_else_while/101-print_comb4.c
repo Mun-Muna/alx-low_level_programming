5c3226e3f27228db7ba8df79a8085494a3b1a19f
Mona <munaaljaali9375@gmail.com>
2023-09-10 15:03:24 -0700

    101-print_comb4

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ * main - entry point to program
+ * Return: exit program
+ */
+
+int main(void)
+{
+	int i;
+	int j;
+	int k;
+
+	for (i = 48; i <= 55; i++)
+	{
+		for (j = (i + 1); j <= 56; j++)
+		{
+			for (k = (j + 1); k <= 57; k++)
+			{
+				putchar(i);
+				putchar(j);
+				putchar(k);
+				if (i != 55)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
