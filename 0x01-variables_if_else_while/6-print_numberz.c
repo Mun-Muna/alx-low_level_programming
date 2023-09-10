7cd3be35859b8d7b4d57441a841948b3aa30c3c6
Mona <munaaljaali9375@gmail.com>
2023-09-10 09:28:25 -0700

    6-print_numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point to the program
+ * Return: Exit program
+ */
+
+int main(void)
+{
+	char ch;
+
+	for (ch = '0'; ch <= '9'; ch++)
+		putchar(ch);
+	
+	putchar('\n');
+	return (0);
+}
