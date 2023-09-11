9393bbbc2f246a83e1e16c13cd5e15286322d476
Mona <munaaljaali9375@gmail.com>
2023-09-10 19:39:23 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,2 +1,16 @@
-#!/bin/bash
-k
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Return: exit
+ */
+
+int main(void)
+{
+	printf("Size of a char: 1 byte(s)\n");
+	printf("Size of an int: 4 byte(s)\n");
+	printf("Size of a long int: 4 byte(s)\n");
+	printf("Size of a long long int: 8 byte(s)\n");
+	printf("Size of a float: 4 byte(s)\n");
+	return (0);
+}
