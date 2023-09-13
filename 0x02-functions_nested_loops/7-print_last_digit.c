3f55dc661eff4d58fcaba45179c15993dd762666
Mona <munaaljaali9375@gmail.com>
2023-09-12 19:07:14 -0700

    Print: Last digit of number

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -9,15 +9,19 @@
 
 int print_last_digit(int n)
 {
+	int m;
+
 	if (n < 0)
 	{
-		_putchar(-n % 10 + '0');
-		return (-n);
+		m = (-1 * n) % 10;
+		_putchar(m  + '0');
+		return (m);
 	}
 	else
-	{	
-		_putchar(n % 10 + '0');
-		return (n);
+	{
+		m = n % 10;
+		_putchar(m + '0');
+		return (m);
 	}
 	return (0);
 }
