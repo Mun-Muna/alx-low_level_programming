0b281c5fc61d4cf6048c0cb9ae92fafa28b638c7
Mona <munaaljaali9375@gmail.com>
2023-09-14 14:08:23 -0700

    Print: Fizz-Buzz test

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0)
+		{
+			printf("%s", "Fizz");
+		}
+		else if (i % 5 == 0)
+		{
+			printf("%s", "Buzz");
+		}
+		else if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf("%s", "FizzBuzz");
+		}
+		else
+		{
+			printf("%d", i);
+		}
+		printf("%c", ' ');
+	}
+	printf("%c", '\n');
+	return (0);
+}
