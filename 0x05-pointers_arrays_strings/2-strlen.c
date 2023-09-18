8c87dec862c06e91fe13aa94433f98a9844e8621
Mona <munaaljaali9375@gmail.com>
2023-09-18 13:14:58 -0700

    Write: a function that returns the length of a string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+/**
+ * _strlen - Returns length of a string
+ * @s: string
+ *
+ * Return: Integer
+ */
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+	}
+	return (i);
+}
