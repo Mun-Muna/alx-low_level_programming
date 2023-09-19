47d86df6c8334ba4195bebef8174ae2069d03316
Mona <munaaljaali9375@gmail.com>
2023-09-18 19:59:53 -0700

    1

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,18 @@
+/**
+ * rev_string - Reverses a string
+ * @s: String to be reversed
+ */
+void rev_string(char *s)
+{
+	int i, j;
+	char x;
+
+	x = s[j]; 
+	for (i = 0; s[i] != '\0'; i++)
+	{
+	}
+	for (j = i - 1; j >= 0; j--)
+	{
+		x = s[j];
+	}
+}
