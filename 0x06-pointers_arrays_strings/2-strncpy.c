669925c3b702e8f76ff26bb8376655c72785c1eb
Mona <munaaljaali9375@gmail.com>
2023-09-20 16:11:56 -0700

    fix: checker error

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -9,15 +9,13 @@
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-	int i, j;
+	int j;
 
-	for (i = 0; src[i] != '\0'; i++)
-	{}
 	for (j = 0; n > 0 && j < n && src[j] != '\0'; j++)
 	{
 		dest[j] = src[j];
 	}
-	if (n > i)
+	if (j < n)
 	{
 		dest[j] = '\0';
 	}
