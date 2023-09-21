74924b4be666a6c6119c9dbc1558a7dbd5a9442f
Mona <munaaljaali9375@gmail.com>
2023-09-21 14:24:32 -0700

    fix: betty error

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -10,10 +10,10 @@ char *rot13(char *s)
 	char a[] = "AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz";
 	char b[] = "NnOoPpQqRrSsTtUuVvWwXxYyZzAaBbCcDdEeFfGgHhIiJjKkLlMm";
 
-	for(i = 0; s[i] != '\0'; i++)
+	for (i = 0; s[i] != '\0'; i++)
 	{
 		for (j = 0; a[j] != '\0'; j++)
-		{	
+		{
 			if (s[i] == a[j])
 			{
 				s[i] = b[j];
