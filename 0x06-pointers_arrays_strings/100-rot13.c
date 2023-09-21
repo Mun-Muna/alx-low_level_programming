2ff78e3d7499e4c535d1fb982cf411f7a13fbf18
Mona <munaaljaali9375@gmail.com>
2023-09-21 14:22:41 -0700

    encode: a string using rot13

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,24 @@
+/**
+ * rot13 - encode a string using rot13
+ * @s: pointer to the string to be encoded
+ *
+ * Return: pointer to a string
+ */
+char *rot13(char *s)
+{
+	int i, j;
+	char a[] = "AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz";
+	char b[] = "NnOoPpQqRrSsTtUuVvWwXxYyZzAaBbCcDdEeFfGgHhIiJjKkLlMm";
+
+	for(i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; a[j] != '\0'; j++)
+		{	
+			if (s[i] == a[j])
+			{
+				s[i] = b[j];
+			}
+		}
+	}
+	return (s);
+}
