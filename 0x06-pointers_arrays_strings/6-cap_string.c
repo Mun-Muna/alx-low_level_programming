f6751cc1d287a2c871c4960ed47b44d8a9f9f074
Mona <munaaljaali9375@gmail.com>
2023-09-22 10:25:19 -0700

    fix: checker error

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -12,13 +12,13 @@ char *cap_string(char *s)
 	i = 0;
 	while (s[i] != '\0')
 	{
-		if (s[i] >= 'a' && s[i] <= 'z')
+		for (j = 0; c[j] != '\0'; j++)
 		{
-			for (j = 0; c[j] != '\0'; j++)
+			if (s[i] == c[j])
 			{
-				if (s[i - 1] == c[j])
+				if (s[i + 1] >= 'a' && s[i + 1] <= 'z')
 				{
-					s[i] = s[i] - 32;
+					s[i + 1] = s[i + 1] - 32;
 				}
 			}
 		}
