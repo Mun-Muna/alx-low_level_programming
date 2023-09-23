1c51d44f8302b041d86fe17ca6f08b6913f7982a
Mona <munaaljaali9375@gmail.com>
2023-09-23 09:15:39 -0700

    Fix: checker error

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -12,15 +12,12 @@ void print_number(int n)
 		_putchar('-');
 		n = -n;
 	}
-	for (i = 10;; i *= 10)
+	for (i = 10; (n / i) > 0; i *= 10)
 	{
-		if (n / i == 0)
-		{
-			break;
-		}
 	}
-	for (i /= 10; i >= 1; i /= 10 )
+	i = i / 10;
+	for (; i >= 1; i /= 10)
 	{
-		_putchar((n / i) % 10 + '0');
+		_putchar(((n / i) % 10) + '0');
 	}
 }
