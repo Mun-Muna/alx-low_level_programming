a7c58778a93563dd80531f0aa64dae7e47be0b3d
Mona <munaaljaali9375@gmail.com>
2023-09-23 17:43:18 -0700

    print: sum of even-valued terms

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+	long x, y, z, sum;
+
+	x = 1;
+	y = 2;
+	sum = 0;
+	for (i = 0; z <= 4000000; i++)
+	{
+		z = x + y;
+		x = y;
+		y = z;
+		if (z % 2 == 0)
+			sum += z;
+	}
+	printf("%ld\n", z);
+	return (0);
+}
