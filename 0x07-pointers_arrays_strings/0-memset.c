ae0a681066272154ada1911a3fb827805055fb51
Mona <munaaljaali9375@gmail.com>
2023-09-25 15:35:18 -0700

    fix: betty error

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,5 +1,5 @@
 /**
- * _memset - fills the memory with a constant byte 
+ * _memset - fills the memory with a constant byte
  * @s: pointer to memory area
  * @b: constant byte
  * @n: number of bytes of the memory area
