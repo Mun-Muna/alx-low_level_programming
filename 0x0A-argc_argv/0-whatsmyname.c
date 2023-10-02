dcc463b95477a55572b90d40957bc719561f59f0
Mona <munaaljaali9375@gmail.com>
2023-10-02 10:15:23 -0700

    fix: betty error

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -3,9 +3,11 @@
  * main - print name of the program
  * @argc: type integer
  * @argv: pointer type char
+ *
+ * Return: 0
  */
 int main(int argc, char *argv[])
 {
-	printf("%s\n",argv[argc - 1]);
+	printf("%s\n", argv[argc - 1]);
 	return (0);
 }
