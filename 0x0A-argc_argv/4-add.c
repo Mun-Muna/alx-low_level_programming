ec153cd4aa4e48da67de217236bca024d81f558c
Mona <munaaljaali9375@gmail.com>
2023-10-02 12:40:21 -0700

    add: positive numbers

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - print name of the program
+ * @argc: type integer
+ * @argv: pointer type char
+ *
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	long int total = 0, i;
+
+	for (i = 1; i < argc; i++)
+	{
+		if (!atoi(argv[i]))
+		{		
+			printf("Error\n");
+			return (1);
+		}
+		else if (atoi(argv[i]) > 0)
+		{
+			total += atoi(argv[i]);
+		}
+	}
+	printf("%ld\n", total);
+	return (0);
+}
