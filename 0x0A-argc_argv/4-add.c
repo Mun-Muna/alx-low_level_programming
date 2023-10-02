23dfc965a452cb2d41457f785a3f768adbe5ed6f
Mona <munaaljaali9375@gmail.com>
2023-10-02 12:48:43 -0700

    fix: checker error

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -13,12 +13,12 @@ int main(int argc, char *argv[])
 
 	for (i = 1; i < argc; i++)
 	{
-		if (atoi(argv[i]) <= 0)
+		if (!atoi(argv[i]))
 		{
 			printf("Error\n");
 			return (1);
 		}
-		else
+		else if (atoi(argv[i]) > 0)
 		{
 			total += atoi(argv[i]);
 		}
