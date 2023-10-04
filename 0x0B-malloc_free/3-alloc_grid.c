829241a70d37d43391d313bd03bd5ee58d786547
Mona <munaaljaali9375@gmail.com>
2023-10-04 13:14:57 -0700

    fix checker

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -9,12 +9,12 @@
 int **alloc_grid(int width, int height)
 {
 	int **grid, *ptr, i, j;
-	
+
 	if (width <= 0 || height <= 0)
 		return (NULL);
 	grid = malloc((width * height * sizeof(int)) + (height * sizeof(int)));
-	ptr = (int*) (grid + height);
-	for(i = 0; i < height; i++)
+	ptr = (int *)(grid + height);
+	for (i = 0; i < height; i++)
 	{
 		grid[i] = (ptr + width * i);
 	}
