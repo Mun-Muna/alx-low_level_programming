7c38e1805b16208405fa68b7a6ace458943701bb
Mona <munaaljaali9375@gmail.com>
2023-10-10 14:31:32 -0700

    fix checker error

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -11,12 +11,7 @@ void print_dog(struct dog *d)
 		return;
 	if (d->name == NULL)
 		d->name = "(nil)";
-	printf("Name: %s\n", d->name);
-	if (d->age == 0)
-		printf("Age: (nil)\n");
-	else
-		printf("Age: %.6f\n", d->age);
 	if (d->owner == NULL)
 		d->owner = "(nil)";
-	printf("Owner: %s\n", d->owner);
+	printf("Name: %s\nAge: %.6f\nOwner: %s\n", d->name, d->age, d->owner);
 }
