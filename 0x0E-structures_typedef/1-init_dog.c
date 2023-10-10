edcab5f273802fe007088ce815ec8387e9c75c80
Mona <munaaljaali9375@gmail.com>
2023-10-10 14:38:50 -0700

    fix checker error

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,4 +1,5 @@
 #include "dog.h"
+#include <stddef.h>
 /**
  * init_dog - initialize a variable of type struct dog
  * @d: type struct dog
@@ -8,9 +9,10 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	if (d == NULL)
-		return;
-	d->name = name;
-	d->age = age;
-	d->owner = owner;
+	if (d != NULL)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
 }
