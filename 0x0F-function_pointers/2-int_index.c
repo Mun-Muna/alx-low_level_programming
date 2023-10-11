bd8b05a53d3d0a62f4067b00dd4baf16d2ec37c7
Mona <munaaljaali9375@gmail.com>
2023-10-11 13:31:18 -0700

    fix checker error

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -13,15 +13,12 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
-	if (array || cmp)
+	if (!array || !cmp)
+		return (-1);
+	for (i = 0; i < size; i++)
 	{
-		for (i = 0; i < size; i++)
-		{
-			if (cmp(array[i]))
-				return (i);
-		}
-		return (-1);
+		if (cmp(array[i]))
+			return (i);
 	}
-	else
-		return (-1);
+	return (-1);
 }
