e276c5ea0549b96e88b7f232f4311cb1b494c512
Mona <munaaljaali9375@gmail.com>
2023-10-11 18:47:49 -0700

    give it a try

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - entry point
+ * @argc: number of arguments
+ * @argv: pointer to array of arguments
+ *
+ * Return: 0 if success.
+ */
+int main(void)
+{
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	if (atoi(argv[1]) < 0)
+	{
+		printf("Error\n");
+		return (2);
+	}
+}
