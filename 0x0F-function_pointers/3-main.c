6b0f825f5d90d6ece5b342f2e0f1d1ffb00c1892
Mona <munaaljaali9375@gmail.com>
2023-10-11 18:11:10 -0700

    fix checker error

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -25,14 +25,15 @@ int main(int argc, char *argv[])
 		exit(99);
 	}
 
-	if ((argv[2][0] == '/' || argv[2][0] == '%') && argv[3] == 0)
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
+
+	if ((argv[2][0] == '/' || argv[2][0] == '%') && num2 == 0)
 	{
 		printf("Error\n");
 		exit(100);
 	}
 
-	num1 = atoi(argv[1]);
-	num2 = atoi(argv[3]);
 	printf("%d\n", (get_op_func(argv[2]))(num1, num2));
 	return (0);
 }
