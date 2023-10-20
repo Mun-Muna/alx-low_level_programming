5be495f8a431508b599f9728b69f288e557acc50
Mona <munaaljaali9375@gmail.com>
2023-10-19 19:27:29 -0700

    fix checker error

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -2,6 +2,6 @@
 void print1(void) __attribute__((constructor));
 void print1(void)
 {
-	printf("You're beat! and yet, you must allow,\n
-			I bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
 }
