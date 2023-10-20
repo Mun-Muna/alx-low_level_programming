4394ba9c2606a37e218252c6d3ba27d49f8b7b28
Mona <munaaljaali9375@gmail.com>
2023-10-19 17:16:44 -0700

    add: code if str is null

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -13,7 +13,7 @@ list_t *add_node(list_t **head, const char *str)
 {
 	list_t *ptr = malloc(sizeof(list_t));
 
-	if (!ptr)
+	if (!ptr || !str)
 		return (NULL);
 	ptr->str = strdup(str);
 	ptr->len = strlen(str);
