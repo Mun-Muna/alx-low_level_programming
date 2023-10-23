4cd99ca2d958f9670bbec771fe261cf5b3ab2853
Mona <munaaljaali9375@gmail.com>
2023-10-23 14:12:38 -0700

    fix betty error

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -3,7 +3,7 @@
  * listint_len - returns the number of elements in a linked listint_t list
  * @h: pointer to struct
  *
- * Return : number of elements
+ * Return: number of elements
  */
 size_t listint_len(const listint_t *h)
 {
