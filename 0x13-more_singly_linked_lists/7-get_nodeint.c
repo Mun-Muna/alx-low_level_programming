3313cc8e513744cb91f61ea6f78f012e22ad1136
Mona <munaaljaali9375@gmail.com>
2023-10-24 13:03:26 -0700

    fix checker errors

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -8,9 +8,9 @@
  */
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
-	unsigned int i = 0;
+	unsigned int i;
 
-	for (; head; i++)
+	for (i = 0; head; i++)
 	{
 		if (i == index)
 			return (head);
