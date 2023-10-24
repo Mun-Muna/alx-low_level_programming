bc38a43d4c95b3e1ba25a3bf8c3b568ef33f1458
Mona <munaaljaali9375@gmail.com>
2023-10-24 14:57:43 -0700

    fix checker output errors

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -14,7 +14,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	listint_t *next = NULL;
 	unsigned int i;
 
-	if (!new || !*head)
+	if (!new)
 		return (NULL);
 	new->n = n;
 	new->next = NULL;
@@ -23,12 +23,11 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		if (i == idx)
 		{
 			next = *head;
-			*head = new;
+			*head = new
 			new->next = next;
-			new = *head;
 			return (new);
 		}
 		*head = (*head)->next;
 	}
-	return (new);
+	return (NULL);
 }
