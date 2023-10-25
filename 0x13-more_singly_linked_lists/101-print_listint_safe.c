4e778073d100f72ff83f7a46c3cd8957d3333327
Mona <munaaljaali9375@gmail.com>
2023-10-24 19:07:43 -0700

    print linked list with a loop

---
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,30 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * print_listint_safe - prints a listint_t linked list
+ * @head: pointer to struct
+ *
+ * Return: number of nodes if succesful or exit status 98.
+ */
+size_t print_listint_safe(const listint_t *head)
+{
+	const listint_t *current = head;
+	int i; 
+
+	i = 0;
+	while (head)
+	{
+		head = head->next;
+		i++;
+		printf ("%d\n", head->n);
+		while (current)
+		{
+			if (head == current)
+				return (i);
+			else
+				current = current->next;
+		}
+	}
+	exit (98);
+}
