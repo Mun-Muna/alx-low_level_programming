9accec5af6a34334e19a88696eefa8f6eb2b772c
Mona <munaaljaali9375@gmail.com>
2023-10-26 14:17:56 -0700

    fix betty error

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -4,12 +4,12 @@
  * @n: type unsigned long int
  * @m: type unsigned long int
  *
- * Return: number of bits you would need to flip to get 
+ * Return: number of bits you would need to flip to get
  * from one number to another
  */
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
-	unsigned long int xor = n ^ m; 
+	unsigned long int xor = n ^ m;
 	unsigned int count = 0;
 
 	while (xor != 0)
