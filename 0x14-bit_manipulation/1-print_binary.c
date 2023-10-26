e8ccde3be46c8930abb6f73a736d7c1367b20e56
Mona <munaaljaali9375@gmail.com>
2023-10-26 12:17:26 -0700

    fix checker error by changing type of rem to double

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -5,18 +5,16 @@
  */
 void print_binary(unsigned long int n)
 {
-	float rem = n * 0.5; 
+	double rem = n * 0.5;
 	unsigned long int num = n * 0.5;
 
 	if (n == 0)
 	{
 		_putchar('0');
-		return;
 	}
 	else if (n == 1)
 	{
 		_putchar('1');
-		return;
 	}
 	else
 	{
