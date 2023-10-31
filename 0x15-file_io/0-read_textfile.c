d155dc26215e3f63a790be7f75a7d10de47bdb59
Mona <munaaljaali9375@gmail.com>
2023-10-30 19:37:25 -0700

    fix: betty error

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -2,11 +2,11 @@
 #include <stdlib.h>
 #include<fcntl.h>
 /**
- * read_textfile - 
- * @filename:
- * @letters:
+ * read_textfile - reads a text file and prints it to the POSIX standard output 
+ * @filename: pointer to file
+ * @letters: number of letters it should read and print
  *
- * Return:
+ * Return: actual number of letters it could read and print.
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
