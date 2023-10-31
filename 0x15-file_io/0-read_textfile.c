4bcbfcc0a0697cdaaf36244f1a45457b404c6372
Mona <munaaljaali9375@gmail.com>
2023-10-30 19:26:59 -0700

    read: a text file and print it to the POSIX standard output

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,34 @@
+#include <unistd.h>
+/**
+ * read_textfile - 
+ * @filename:
+ * @letters:
+ *
+ * Return:
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	ssize_t open_file, read_file, write_file;
+	char *p;
+
+	if (filename == NULL)
+		return (0);
+
+	p = malloc(sizeof(char) * letters);
+	if (p == NULL)
+		return (0);
+
+	open_file = open(filename, O_RDONLY);
+	read_file = read(open_file, p, letters);
+	write_file = write(1, p, read_file);
+
+	if (open_file == -1 || read_file == -1)
+	{
+		free(p);
+		return (0);
+	}
+	close(open_file);
+	free(p);
+	return (write_file);
+}
+
