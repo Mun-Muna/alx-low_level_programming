90dbad4741f6849613eb8ed8a793efbc9c175385
Mona <munaaljaali9375@gmail.com>
2023-12-07 12:00:31 -0800

    [Fix]: betty error

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -13,7 +13,7 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	dlistint_t *new = malloc(sizeof(dlistint_t));
 
 	if (!new)
-		return NULL;
+		return (NULL);
 	new->n = n;
 	new->next = *head;
 	new->prev = NULL;
