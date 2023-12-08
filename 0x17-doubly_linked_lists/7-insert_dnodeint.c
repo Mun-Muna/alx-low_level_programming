6ed05ba9f94e611dd342651ca00f136d818ef0ba
Mona <munaaljaali9375@gmail.com>
2023-12-07 17:46:59 -0800

    still trying

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -14,7 +14,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	dlistint_t *current = *h;
 	unsigned int i;
 
-	if (!ne)
+	if (!new)
 		return (NULL);
 	if (idx == 0)
 		return (add_dnodeint(h, n));
