b17f9ed86d6aaca39aefab2a70d13831a61baf36
Mona <munaaljaali9375@gmail.com>
2023-12-07 17:33:22 -0800

    [fix]: output error

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -14,21 +14,25 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	dlistint_t *current = *h;
 	unsigned int i;
 
+	if (!new)
+		return NULL;
 	if (idx == 0)
 		return (add_dnodeint(h, n));
-	for (i = 1; (current)->next; i++)
+	if (!(current->next))
+		return (add_dnodeint_end(h, n));
+	for (i = 1; current; i++)
 	{
+		current = current->next;
 		if (i == idx)
 		{
-			if ((current)->next == NULL)
-				return (add_dnodeint_end(h, n));
 			new->n = n;
-			new->next = current->next;
-			new->prev = current;
-			current->next = new;
+			new->next = current;
+			new->prev = current->prev;
+			current->prev->next = new;
+			current->prev = new;
 			return (new);
 		}
-		current = current->next;
 	}
+	free(new);
 	return (NULL);
 }
